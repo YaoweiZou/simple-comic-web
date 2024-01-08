999620e8090de64509b6835f4de06ada2498c97e
Yaowei <yaoweipublic@icloud.com>
2024-01-08 21:18:22 +0800

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/simple-comic-web/",
   plugins: [preact()],
   server: {
     port: 3000,
